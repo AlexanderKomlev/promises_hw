e0f656cba4e4796d19e8c571ac13d5c634c3f83b
Alexander Komlev <alexander.komlev42@gmail.com>
2024-08-03 16:17:48 +0700

    fix app.js

--- src/app.js
+++ src/app.js
@@ -4,9 +4,14 @@ import GameSaving from './gamesaving';
 
 export default class GameSavingLoader {
   static load() {
-    return read().then(json).then((response) => {
-      const data = JSON.parse(response);
-      return new GameSaving(data.id, data.created, data.userInfo);
-    });
+    return read()
+      .then(json)
+      .then((response) => {
+        const data = JSON.parse(response);
+        return new GameSaving(data.id, data.created, data.userInfo);
+      })
+      .catch((error) => {
+        throw new Error(error);
+      });
   }
 }
